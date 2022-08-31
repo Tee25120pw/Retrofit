5b006c675ac70cf5f0e314d093371800e08518df
Woramet.K <waramet.k@gofive.co.th>
2022-08-31 19:09:30 +0700

    Main

--- app/src/main/java/com/example/retrofit/ViewModel.kt
+++ app/src/main/java/com/example/retrofit/ViewModel.kt
@@ -1,39 +1,39 @@
-package com.example.retrofit//package com.example.retrofit
-
-import android.util.Log.d
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
-import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
-lateinit var myAdapter: MyAdapter
-const val Base_Url = "https://jsonplaceholder.typicode.com/"
-open class ViewModel  : ViewModel() {
-    fun getMyData() {
-
-        val retrofitBuilder = Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl(Base_Url)
-            .build()
-            .create(ApiInterface::class.java)
-
-        val retrofitData = retrofitBuilder.getData()
-
-        retrofitData.enqueue(object : Callback<List<MyDataItem>?> {
-            override fun onResponse(call: Call<List<MyDataItem>?>, response: Response<List<MyDataItem>?>) {
-                val responseBody = response.body()!!
-                myAdapter = MyAdapter(baseContext, responseBody)
-                myAdapter.notifyDataSetChanged()
-                recycleview_users.adapter = myAdapter
-            }
-
-            override fun onFailure(call: Call<List<MyDataItem>?>, t: Throwable) {
-                d("ViewModel", "onFailure: " + t.message)
-            }
-        })
-    }
-}
+//package com.example.retrofit//package com.example.retrofit
+//
+//import android.util.Log.d
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.lifecycle.ViewModel
+//import kotlinx.android.synthetic.main.activity_main.*
+//import retrofit2.Call
+//import retrofit2.Callback
+//import retrofit2.Response
+//import retrofit2.Retrofit
+//import retrofit2.converter.gson.GsonConverterFactory
+//
+//lateinit var myAdapter: MyAdapter
+//const val Base_Url = "https://jsonplaceholder.typicode.com/"
+//open class ViewModel  : ViewModel() {
+//    fun getMyData() {
+//
+//        val retrofitBuilder = Retrofit.Builder()
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .baseUrl(Base_Url)
+//            .build()
+//            .create(ApiInterface::class.java)
+//
+//        val retrofitData = retrofitBuilder.getData()
+//
+//        retrofitData.enqueue(object : Callback<List<MyDataItem>?> {
+//            override fun onResponse(call: Call<List<MyDataItem>?>, response: Response<List<MyDataItem>?>) {
+//                val responseBody = response.body()!!
+//                myAdapter = MyAdapter(baseContext, responseBody)
+//                myAdapter.notifyDataSetChanged()
+//                recycleview_users.adapter = myAdapter
+//            }
+//
+//            override fun onFailure(call: Call<List<MyDataItem>?>, t: Throwable) {
+//                d("ViewModel", "onFailure: " + t.message)
+//            }
+//        })
+//    }
+//}
