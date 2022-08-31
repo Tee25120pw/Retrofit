23abcf3a1ca69e3f3f7d8a53619055ccbe18d861
Woramet.K <waramet.k@gofive.co.th>
2022-08-31 20:02:26 +0700

    [Update] no bug in T

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -1,23 +1,15 @@
-package com.example.retrofit
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log.d
-import androidx.lifecycle.ViewModel
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.retrofit.MyAdapter
+import com.example.retrofit.R
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
-const val Base_Url = "https://jsonplaceholder.typicode.com/"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: ViewModel
 
-//    private lateinit var viewModel: ViewModel
+    //    private lateinit var viewModel: ViewModel
     lateinit var myAdapter: MyAdapter
     lateinit var linearLayoutManager: LinearLayoutManager
 
@@ -25,39 +17,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        viewModel = ViewModelProvider(this).get(ViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(ViewModel::class.java)
         recycleview_users.setHasFixedSize(true)
         linearLayoutManager = LinearLayoutManager(this)
         recycleview_users.layoutManager = linearLayoutManager
 
-        getMyData()
+        viewModel.getMyData()
     }
-
-    private fun getMyData() {
-        val retrofitBuilder = Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl(Base_Url)
-            .build()
-            .create(ApiInterface::class.java)
-
-        val retrofitData = retrofitBuilder.getData()
-
-        retrofitData.enqueue(object : Callback<List<MyDataItem>?> {
-            override fun onResponse(
-                call: Call<List<MyDataItem>?>,
-                response: Response<List<MyDataItem>?>
-            ) {
-                val responseBody = response.body()!!
-
-                myAdapter = MyAdapter(baseContext, responseBody)
-                myAdapter.notifyDataSetChanged()
-                recycleview_users.adapter = myAdapter
-            }
-
-            override fun onFailure(call: Call<List<MyDataItem>?>, t: Throwable) {
-                d("MainActivity", "onFailure: " + t.message)
-            }
-        })
-    }
-
 }
+//    private fun getMyData() {
+//        val retrofitBuilder = Retrofit.Builder()
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .baseUrl(Base_Url)
+//            .build()
+//            .create(ApiInterface::class.java)
+//
+//        val retrofitData = retrofitBuilder.getData()
+//
+//        retrofitData.enqueue(object : Callback<List<MyDataItem>?> {
+//            override fun onResponse(call: Call<List<MyDataItem>?>, response: Response<List<MyDataItem>?>) {
+//                val responseBody = response.body()!!
+//
+//                myAdapter = MyAdapter(baseContext, responseBody)
+//                myAdapter.notifyDataSetChanged()
+//                recycleview_users.adapter = myAdapter
+//            }
+//
+//            override fun onFailure(call: Call<List<MyDataItem>?>, t: Throwable) {
+//                d("MainActivity", "onFailure: " + t.message)
--- app/src/main/java/com/example/retrofit/ViewModel.kt
+++ app/src/main/java/com/example/retrofit/ViewModel.kt
@@ -1,39 +1,47 @@
-//package com.example.retrofit//package com.example.retrofit
-//
-//import android.util.Log.d
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.lifecycle.ViewModel
-//import kotlinx.android.synthetic.main.activity_main.*
-//import retrofit2.Call
-//import retrofit2.Callback
-//import retrofit2.Response
-//import retrofit2.Retrofit
-//import retrofit2.converter.gson.GsonConverterFactory
-//
-//lateinit var myAdapter: MyAdapter
-//const val Base_Url = "https://jsonplaceholder.typicode.com/"
-//open class ViewModel  : ViewModel() {
-//    fun getMyData() {
-//
-//        val retrofitBuilder = Retrofit.Builder()
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .baseUrl(Base_Url)
-//            .build()
-//            .create(ApiInterface::class.java)
-//
-//        val retrofitData = retrofitBuilder.getData()
-//
-//        retrofitData.enqueue(object : Callback<List<MyDataItem>?> {
-//            override fun onResponse(call: Call<List<MyDataItem>?>, response: Response<List<MyDataItem>?>) {
-//                val responseBody = response.body()!!
-//                myAdapter = MyAdapter(baseContext, responseBody)
-//                myAdapter.notifyDataSetChanged()
-//                recycleview_users.adapter = myAdapter
-//            }
-//
-//            override fun onFailure(call: Call<List<MyDataItem>?>, t: Throwable) {
-//                d("ViewModel", "onFailure: " + t.message)
-//            }
-//        })
-//    }
-//}
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.example.retrofit.ApiInterface
+import com.example.retrofit.MyAdapter
+import com.example.retrofit.MyDataItem
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+const val Base_Url = "https://jsonplaceholder.typicode.com/"
+open class ViewModel : ViewModel() {
+
+//    lateinit var linearLayoutManager: LinearLayoutManager
+
+    fun getMyData() {
+        lateinit var myAdapter: MyAdapter
+
+        val retrofitBuilder = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(Base_Url)
+            .build()
+            .create(ApiInterface::class.java)
+
+        val retrofitData = retrofitBuilder.getData()
+
+
+        retrofitData.enqueue(object : Callback<List<MyDataItem>?> {
+            override fun onResponse(
+                call: Call<List<MyDataItem>?>,
+                response: Response<List<MyDataItem>?>
+            ) {
+                val responseBody = response.body()!!
+
+//                    myAdapter = MyAdapter(baseContext, responseBody)
+//                    myAdapter.notifyDataSetChanged()
+//                    recycleview_users.adapter = myAdapter
+            }
+
+            override fun onFailure(call: Call<List<MyDataItem>?>, t: Throwable) {
+                Log.d("MainActivity", "onFailure: " + t.message)
+            }
+        })
+    }
+
+}
